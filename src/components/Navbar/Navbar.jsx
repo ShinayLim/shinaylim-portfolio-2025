34438c79e8d21b04ae4e706bc3dc5fa00af05b94
Shin I. Lim <92615780+ShinayLim@users.noreply.github.com>
2025-02-05 21:53:24 +0800

    navbar styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import styles from './Navbar.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+   <>
+   <nav className={styles.navbar}>
+    <a href="/" className={styles.title}>Portfolio</a>
+    <div className={styles.menu}>
+    <FontAwesomeIcon icon={faBars} fontSize="25px" color='white'alt="menuBtn"/>
+
+        <ul className={styles.menuItems}>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#experience">Experience</a>
+            </li>
+            <li>
+                <a href="#projects">Projects</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
     </div>
+   </nav>
+   </>
   )
 }
 
